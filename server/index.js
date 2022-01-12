efec1c5fabfb0fbbd15ca2c5e88943841ed3bdc7
Luke Eltiste <luke343279@gmail.com>
2022-01-12 09:47:34 -0800

    API for adding data to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
 const path = require("path");
+var MongoClient = require("mongodb").MongoClient;
 
 const PORT = 4000;
 const DEFAULT_LIMIT = 50;
@@ -24,6 +26,133 @@ connection.connect((err) => {
 });
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+MongoClient.connect("mongodb://localhost:27017/dredge", {
+  useUnifiedTopology: true,
+})
+  .then((client) => {
+    console.log("Connected to MongoDB");
+    const db = client.db("dredge");
+
+    app.post("/api/v1/dredge/data", (req, res) => {
+      const { content, dredge } = req.body;
+
+      const events = content.DQM_Data.messages;
+
+      var work_event = events.filter((event) => {
+        const key = Object.keys(event)[0];
+        return key.includes("work_event");
+      });
+
+      var non_eff_event = events.filter((event) => {
+        const key = Object.keys(event)[0];
+        return key.includes("non_eff_event");
+      });
+
+      var remaining = events.filter((event) => {
+        const key = Object.keys(event)[0];
+        return !key.includes("non_eff_event") && !key.includes("work_event");
+      });
+
+      work_event = work_event[0];
+      non_eff_event = non_eff_event[0];
+
+      if (work_event) {
+        const collection_name = `${dredge
+          .toLowerCase()
+          .replace(" ", "_")}.work_event`;
+
+        const collection = db.collection(collection_name);
+        const data = work_event.work_event;
+
+        collection
+          .insertOne({
+            ...data,
+            msg_time: new Date(Date.parse(data.msg_time + " UTC")),
+            comment: data.comment.trim(),
+          })
+          .then((result) => {
+            console.log(result);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+
+      if (non_eff_event) {
+        const collection_name = `${dredge
+          .toLowerCase()
+          .replace(" ", "_")}.ne_event`;
+
+        const collection = db.collection(collection_name);
+        const data = non_eff_event.non_eff_event;
+
+        collection
+          .insertOne({
+            msg_start_time: new Date(Date.parse(data.msg_start_time + " UTC")),
+            msg_end_time: new Date(Date.parse(data.msg_end_time + " UTC")),
+            function_code: data.function_code.trim(),
+            comment: data.comment.trim(),
+          })
+          .then((result) => {
+            console.log(result);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+
+      if (remaining.length > 0) {
+        remaining.forEach((element) => {
+          const collection_name = `${dredge
+            .toLowerCase()
+            .replace(" ", "_")}.other`;
+
+          const collection = db.collection(collection_name);
+          collection
+            .insertOne(element)
+            .then((result) => {
+              console.log(result);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        });
+      }
+
+      res.json({
+        status: "success",
+      });
+    });
+
+    app.post("/api/v1/dredge/data_extra", (req, res) => {
+      const { content, dredge } = req.body;
+
+      const collection_name = `${dredge
+        .toLowerCase()
+        .replace(" ", "_")}.extra_data`;
+
+      const collection = db.collection(collection_name);
+      collection
+        .insertOne({
+          ...content,
+          timestamp: new Date(Date.parse(content.timestamp + " UTC")),
+        })
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+
+      res.json({
+        status: "success",
+      });
+    });
+  })
+  .catch((error) => console.error(error));
 
 app.get("/api/dredges", (req, res) => {
   connection.query("SELECT * FROM work_event", (err, results) => {
